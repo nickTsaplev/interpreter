90854c70b9a376c0c7fe4b1fa9428a9a9c210b01
Nikita Aksenov <naksenov0513@gmail.com>
2026-09-21 13:32:56 +0300

    Revert "Rewrite tests and add new ones"

---
+++ app/src/test/kotlin/ListIOMock.kt
@@ -0,0 +1,13 @@
+import ru.tsaplev.app.execution.IntIO
+
+class ListIOMock(val input: List<Int>,
+                 val output: MutableList<Int?> = mutableListOf<Int?>()) : IntIO {
+    private var count = 0
+    override fun write(output: Int?) {
+        this.output.add(output)
+    }
+
+    override fun read(): Int {
+        return input[count++]
+    }
+}
